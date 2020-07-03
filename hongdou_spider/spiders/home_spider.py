cf01e843affaf24e9572f12156f18345fce69bc3
han720 <han720>
2020-07-02 17:05:23 -0700

    Please review the test code :)

--- hongdou_spider/spiders/home_spider.py
+++ hongdou_spider/spiders/home_spider.py
@@ -1,6 +1,7 @@
 import scrapy
 from scrapy.spiders import CrawlSpider
 from scrapy.http import FormRequest
+from scrapy.http import Request
 
 HOME_URL = [
     'https://www.2redbeans.com/zh-CN/app/login',
@@ -24,3 +25,12 @@ class RbSpider(CrawlSpider):
     def parse(self, response):
         cookie = response.headers.getlist('Set-Cookie')
         self.log(cookie)
+        print(response.body)
+        # self.log(response.body)
+
+        # send request
+        request_with_cookies = Request(callback=self.parse, url="https://www.2redbeans.com/zh-CN/app/search", cookies={'_dating_session':'a3pVZ1R5MjBrUnlHVXRsSjAzdFh1ZkdYN0lscVNTaUdNMFc0N1pZWldOMys5NGRTd2NtV0hYYWtkTmJvZmJsSG9UbjVzK0VhSnJrV0xoQ0JLWlRlZnJQWmxIdUZrVzBMN1dJUytZcTRzTHFHNnB0Z093SWJiZ2hLdVhEUE85TnhsQ1lJb1ZoR05sWmtQeW5YTERxcDBmTExjeEpJWGtQQ0lhSVowSXRaOTZvL1FkaUUxWkhzNmRlS1NSWWNuY25XUGpsT092RzF6OW1HMUdCRFFKRmJTMksrVTVLK1plMVY3Y0FVYS8xRVQyWFBDK01XR3BLdUNEM1YzVnhVUHBVTnVJV1JVVFNqdzUwRDlENmNFa0c1Wk9hTk1YTjVPZjlxQVJEdGpVaXJxbEpmb2NoWjdQTDlRY0U3SmFFSytVZHFsSiszK1ZUWTFlWlJibFZOQ1FRQXV2UVYwTG8xRlErWTZ2aDJ2MDJlbkI0PS0tdVdLR1AzS3ZDZVVCdnoxTzEveEhZdz09--4c0d9e36f567f6d2f4248e6ee60ea44bfebff134; path=/; HttpOnly'})
+        yield request_with_cookies
+
+    def parse_1(self, response):
+        self.log(response.body)
